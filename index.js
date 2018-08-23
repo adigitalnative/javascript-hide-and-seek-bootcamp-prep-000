95378e35a01953d5c630a7f19e3307097b836ac6
adigitalnative@gmail.com <adigitalnative@gmail.com>
2018-08-23 20:06:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list');
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children;
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+    }
+  }
+}
+
+function deepestChild() {
+  let current = document.getElementById("grand-node");
+  let next = current.children[0];
+  
+  while (next) {
+    current = next;
+    next = current.children[0];
+  }
+  
+  return current;
+}
